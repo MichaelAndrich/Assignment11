e0da3b704122d4b771ad6541d3086e17fe722f9d
Michael Andrich <michaelandrich@knights.ucf.edu>
2021-09-17 03:50:03 -0400

    Assignment11

---
+++ src/test/java/solution11.java
@@ -0,0 +1,33 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 11 Solution
+ *  Copyright 2021 Michael Andrich
+ */
+/*
+
+    prompt for how many euros user has
+    use formula to convert from euros to us dollars
+    prompt user for the current exchange rate
+    print out new converted amount in us dollars
+
+ */
+
+import java.util.Scanner;
+
+public class solution11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("How many euros are you exchanging? ");
+        String num1 = sc.nextLine();
+        int euro = Integer.parseInt("" + num1);
+
+        System.out.println("What is the exchange rate? ");
+        String num2 = sc.nextLine();
+        int rate = Integer.parseInt("" + num2);
+
+        double convert = (euro * rate);
+
+        System.out.println(""+(euro)+" euros at an exchange rate of "+(rate)+ " is "+(convert)+ "U.S. dollars. ");
+
+    }
+}
